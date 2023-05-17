60ddcf630cf8d4e90734bb48bb4d0c1ba0df7720
Gustavo Mendonça <alf4r6@gmail.com>
2023-05-16 23:35:19 -0300

    web: add stars background

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -7,9 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily : {
+      fontFamily: {
         sans: 'var(--font-roboto)',
-        alt: 'var(--font-bai-jamjuree)'
+        alt: 'var(--font-bai-jamjuree)',
       },
 
       colors: {
@@ -49,6 +49,19 @@ module.exports = {
           800: '#027435',
           900: '#025929',
         },
+      },
+
+      backgroundImage: {
+        stripes:
+          'linear-gradient(to bottom, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1) 12.5%, transparent 12.5%, transparent)',
+      },
+
+      fontSize: {
+        '5xl': '2.5rem',
+      },
+
+      backgroundSize: {
+        stripes: '100% 8px',
       },
 
       blur: {
