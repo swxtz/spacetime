883c0198b58a83430323bcbbb684fab566150451
Gustavo Mendonça <alf4r6@gmail.com>
2023-05-16 23:01:42 -0300

    web: blur background

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,15 +2,22 @@ export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
       {/* Left */}
-      <div className=""></div>
+      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden">
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 bg-purple-700 opacity-50 rounded-full blur-full"></div>
+      </div>
 
       {/* Right */}
       <div className="flex flex-col p-16">
         <div className="flex flex-1 items-center justify-center">
           <p className="text-center leading-relaxed w-[360px]">
-          Você ainda não registrou nenhuma lembrança, comece a{" "}
-          <a href="#" className="underline hover:text-gray-50 transition-colors">criar agora!</a>
-        </p>
+            Você ainda não registrou nenhuma lembrança, comece a{" "}
+            <a
+              href="#"
+              className="underline hover:text-gray-50 transition-colors"
+            >
+              criar agora!
+            </a>
+          </p>
         </div>
       </div>
     </main>
