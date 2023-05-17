5e9b73c6fc2093d7a0ad23a81dd1e7aed6a26a15
Gustavo Mendonça <alf4r6@gmail.com>
2023-05-16 22:37:26 -0300

    web: home right side

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,18 @@
-
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
-      <h1 className="text-xl font-bold ">Sua capsula do tempo</h1>
-      <h1 className="text-xl font-alt ">Sua capsula do tempo</h1>
-    </div>
-  )
+    <main className="grid grid-cols-2 min-h-screen">
+      {/* Left */}
+      <div className=""></div>
+
+      {/* Right */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed w-[360px]">
+          Você ainda não registrou nenhuma lembrança, comece a{" "}
+          <a href="#" className="underline hover:text-gray-50 transition-colors">criar agora!</a>
+        </p>
+        </div>
+      </div>
+    </main>
+  );
 }
