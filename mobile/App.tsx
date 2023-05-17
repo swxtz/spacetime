4f401b306a41dc2345861cdca396959a02f4cda3
Gustavo Mendonça <alf4r6@gmail.com>
2023-05-16 21:29:23 -0300

    mobile: getting starting

---
+++ mobile/App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className='bg-zinc-950 flex-1 items-center justify-center'>
+      <Text className='text-zinc-50 text-4xl'>Hello Native</Text>
+      <StatusBar style="light" translucent />
+    </View>
+  );
+}
+
