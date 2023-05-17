ec0c0c203d5cfea5bedaed47db93df0963bea49a
Gustavo Mendonça <alf4r6@gmail.com>
2023-05-16 22:22:24 -0300

    web: config colors in tailwind

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
       <h1 className="text-xl font-bold ">Sua capsula do tempo</h1>
+      <h1 className="text-xl font-alt ">Sua capsula do tempo</h1>
     </div>
   )
 }
