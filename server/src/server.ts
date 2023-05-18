39693cc49e2c330a02fe203b8f08455dd48cc2a1
Gustavo Mendonça <alf4r6@gmail.com>
2023-05-18 20:17:51 -0300

    server: create cors rules

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,10 +3,13 @@ dotenv.config();
 
 import fastify from "fastify";
 import { memoriesRoutes } from "./routes/memories";
+import cors from "@fastify/cors";
 
 const app = fastify();
 const port = Number(process.env.HOST_PORT) || 3333;
-
+app.register(cors, {
+  origin: true, //Mudar no deploy
+});
 app.register(memoriesRoutes);
 
 app.listen({
