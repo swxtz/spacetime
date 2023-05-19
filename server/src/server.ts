26359009b5a9eda9eb7b7323faf525b9154178cb
Gustavo Mendonça <alf4r6@gmail.com>
2023-05-19 04:54:36 -0300

    server: using jwt for access token

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,13 +4,20 @@ dotenv.config();
 import fastify from "fastify";
 import { memoriesRoutes } from "./routes/memories";
 import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
 import { authRoutes } from "./routes/auth";
 
 const app = fastify();
 const port = Number(process.env.HOST_PORT) || 3333;
+const jwtSecret = process.env.JWT_SECRET_DEV || "spacetime";
 app.register(cors, {
   origin: true, //Mudar no deploy
 });
+
+app.register(jwt, {
+  secret: jwtSecret
+});
+
 app.register(memoriesRoutes);
 app.register(authRoutes);
 
