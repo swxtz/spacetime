f6f16d334deb2585b9d934d75280d76e0601a58d
Gustavo Mendonça <alf4r6@gmail.com>
2023-05-19 01:18:00 -0300

    web: using github oauth

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         {/* Stripes */}
         <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes" />
 
-        <a href="" className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors">
+        <a href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`} className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors">
           <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
             <User className="h-5 w-5 text-gray-500"/>
           </div>
