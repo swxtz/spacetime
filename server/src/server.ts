c2330656878236715201abbe5e5dc65880e13f97
Gustavo Mendonça <alf4r6@gmail.com>
2023-05-19 04:37:04 -0300

    server: create auth access token

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ dotenv.config();
 import fastify from "fastify";
 import { memoriesRoutes } from "./routes/memories";
 import cors from "@fastify/cors";
+import { authRoutes } from "./routes/auth";
 
 const app = fastify();
 const port = Number(process.env.HOST_PORT) || 3333;
@@ -11,6 +12,7 @@ app.register(cors, {
   origin: true, //Mudar no deploy
 });
 app.register(memoriesRoutes);
+app.register(authRoutes);
 
 app.listen({
   port,
